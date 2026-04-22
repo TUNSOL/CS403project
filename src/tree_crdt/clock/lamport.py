c507dfa12810e5a121bab824004872f0003feb1d
Simay Karakış <simay.karakis@sabanciuniv.edu>
2026-04-22 15:26:41 +0300

    tree.py, lamport.py are done, .env and .gitignore is added.

--- src/tree_crdt/clock/lamport.py
+++ src/tree_crdt/clock/lamport.py
@@ -1,5 +1,24 @@
 from . import Clock
 
 class LamportClock(Clock):
-  # TODO: Implement this class by inheriting from Clock
-  pass
+    # constructor  
+    def __init__(self, id: int):
+        self.__id = id
+        self.__timestamp = 0
+
+    # getterrss
+    @property
+    def id(self) -> int:
+        return self.__id
+
+    @property
+    def timestamp(self) -> int:
+        return self.__timestamp
+
+    # if received == None --> local update
+    def update(self, received: int) -> None:
+        self.__timestamp = max(self.__timestamp, received) + 1
+
+    
+    def __str__(self) -> str:
+        return f"LamportClock(id={self.__id}, timestamp={self.__timestamp})"
--- src/tree_crdt/tree/tree.py
+++ src/tree_crdt/tree/tree.py
@@ -1,5 +1,45 @@
 from .node import Node
 
+
 class Tree:
-  # TODO: Implement this class
-  pass
+    def __init__(self):
+        self.__nodes: dict = {}  # child_id -> Node
+
+    def __call__(self) -> set:
+        return set(self.__nodes.values())
+
+    def __getitem__(self, key) -> Node | None:
+        return self.__nodes.get(key, None)
+
+    def _is_ancestor(self, ancestor_id, node_id) -> bool:
+        """Returns True if ancestor_id is an ancestor of node_id in the current tree.
+            This method will be used to detect cycles in move operation."""
+        current_id = node_id
+        while current_id is not None:
+            node = self.__nodes.get(current_id)
+            if node is None:
+                break
+            current_id = node.parent
+            if current_id == ancestor_id:
+                return True
+        return False
+
+    def move(self, new_node: Node) -> None:
+        child_id = new_node.child
+        new_parent_id = new_node.parent
+        existing = self.__nodes.get(child_id) # true if this node already exists in the tree
+
+        if existing is None:
+            self.__nodes[child_id] = new_node
+            return
+
+        # ignore the operation if the new parent is a descendant of the child (cycle!)
+        if new_parent_id is not None and (
+            new_parent_id == child_id or self._is_ancestor(child_id, new_parent_id)
+        ):
+            return
+
+        self.__nodes[child_id] = new_node
+
+    def __str__(self) -> str:
+        return str(self())
